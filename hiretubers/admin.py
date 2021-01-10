ec12d49e8d111ef2b963be7b8d7e377fd9970317
Nikhil Chintawar <chintawarnikhil88@gmail.com>
2021-01-10 18:04:12 +0530

    Customize admin panel

--- hiretubers/admin.py
+++ hiretubers/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import Hiretuber
 
 # Register your models here.
-admin.site.register(Hiretuber)
+@admin.register(Hiretuber)
+class HiretuberAdmin(admin.ModelAdmin):
+    list_display=('id', 'first_name', 'last_name', 'email', 'city')
+    list_display_links=('first_name',)
+    search_fields=('first_name', 'city')
+    ordering=['id']
+
+# admin.site.register(Hiretuber)
