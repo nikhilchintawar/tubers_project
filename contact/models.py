70155305ce8d339199a57864118ed871c47fd029
Nikhil Chintawar <chintawarnikhil88@gmail.com>
2021-01-13 08:51:28 +0530

    contact: Add model & register in admin

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Contact(models.Model):
+    full_name = models.CharField(max_length=100)
+    phone_no = models.IntegerField()
+    email = models.CharField(max_length=100)
+    company_name = models.CharField(max_length=100)
+    subject = models.CharField(max_length=200)
+    message = models.TextField(blank=True)
+    created_date = models.DateTimeField(blank=True, default=datetime.now)
+
+    def __str__(self):
+        return self.email;
