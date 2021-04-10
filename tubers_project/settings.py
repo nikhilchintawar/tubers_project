4c662759cc3e13b69efacb8c77ab749cc4c1e5f4
Nikhil Chintawar <chintawarnikhil88@gmail.com>
2021-04-10 23:27:22 +0530

    navbar: Add active link

--- tubers_project/settings.py
+++ tubers_project/settings.py
@@ -49,8 +49,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'allauth.socialaccount.providers.google',
-    'allauth.socialaccount.providers.facebook'
+    # 'allauth.socialaccount.providers.google',
+    # 'allauth.socialaccount.providers.facebook'
 ]
 
 MIDDLEWARE = [
